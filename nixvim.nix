3b358028ed8b20db289f048de01e248177a2b706
Siesta <20047950+deepsiesta@users.noreply.github.com>
2024-10-24 14:06:49 +0200

    Remove colorscheme

--- nixvim.nix
+++ nixvim.nix
@@ -126,23 +126,23 @@
     # Don't forget to disable the colorschemes you arent using
     #
     # If you want to see what colorschemes are already installed, you can use `:Telescope colorschme`.
-    colorschemes = {
-      # https://nix-community.github.io/nixvim/colorschemes/tokyonight/index.html
-      tokyonight = {
-        enable = true;
-        settings = {
-          # Like many other themes, this one has different styles, and you could load
-          # any other, such as 'storm', 'moon', or 'day'.
-          style = "night";
-          transparent = true;
-          styles = {
-            sidebars = "transparent";
-            floats = "transparent";
-            keywords = {italic = false;};
-          };
-        };
-      };
-    };
+    # colorschemes = {
+    #   # https://nix-community.github.io/nixvim/colorschemes/tokyonight/index.html
+    #   tokyonight = {
+    #     enable = true;
+    #     settings = {
+    #       # Like many other themes, this one has different styles, and you could load
+    #       # any other, such as 'storm', 'moon', or 'day'.
+    #       style = "night";
+    #       transparent = true;
+    #       styles = {
+    #         sidebars = "transparent";
+    #         floats = "transparent";
+    #         keywords = {italic = false;};
+    #       };
+    #     };
+    #   };
+    # };
 
     # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=globals#globals
     globals = {
